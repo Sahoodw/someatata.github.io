3ceb2a612b8956ef601f94a027e8d76ffac918d7
Sahoodw <89397385+Sahoodw@users.noreply.github.com>
2021-09-24 15:01:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// // alert("hello word");
+
+// var a = "Atatat";
+// let b = "barca"
+// const c = false ;
+// let d ;
+// let e = null ;
+// // let f = symbol();
+
+// let arr = [1 ,"sara", [2,2]]
+// let obj = {
+//     userName: 'Pavlo'
+ 
+// };
+
+// alert(a);
+// console.log(a);
+// a = 2000;
+// console.log(a);
+// a = a + 500;
+// console.log(a);
+// a = 'some text' ;
+// console.log(a);
+
+
+let city ="Kiev"
+city="lviv"
+let address=city="lviv"
+alert(city)
+
+let a = 17 ;
+const result = a*a*a ;
+console.log(result)
+
+// let num1 = 6 ;
+// let num2 = num1 ;
+// console.log (num2);
+
+let Ryzka = 5.25;
+let c  = 4 ;
+let Olivets = 3.5 ;
+let b = 6 ;
+let q = c*Ryzka + b*Olivets ;
+console.log(q)
+let r = c + b ;
+console.log(r)
+
+
+
